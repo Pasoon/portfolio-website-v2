14a45b6d2acca88057bc09353fa25c317cf37083
Pasoon Azimi <pasoon.azimi10@gmail.com>
2019-01-12 19:07:13 -0500

    Job descriptions

--- src/constants.js
+++ src/constants.js
@@ -20,7 +20,7 @@ export const WELBI = {
     date: 'Apr 2018 - Aug 2018',
     logo: welbiLogo,
     image: welbiImage,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra dignissim convallis. Phasellus vehicula augue arcu, quis eleifend erat commodo ac. Suspendisse eget euismod est. Ut pharetra volutpat mi sed gravida. Proin ornare elementum scelerisque. Donec sed condimentum lacus. Donec auctor euismod mauris. Phasellus ultrices in velit eu varius. Quisque aliquam odio ex, sed feugiat lorem pellentesque ac. Morbi consectetur dictum blandit.',
+    description: 'As a full stack developer part of a team of five at Welbi I was given the lead on huge features for both mobile and web. With very limited help from peers it was my duty to use all resources at my disposal to complete these tasks. The start-up culture in general requires you to work extremely hard, think on your feet and continually research solutions to all your problems. I had the opportunity to work with React.js, Redux and Router.',
     tasks: [
         {
             title: 'Web Dev',
@@ -55,7 +55,7 @@ export const NOKIA = {
     date: 'Sep 2017 - Dec 2017',
     logo: nokiaLogo,
     image: nokiaImage,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra dignissim convallis. Phasellus vehicula augue arcu, quis eleifend erat commodo ac. Suspendisse eget euismod est. Ut pharetra volutpat mi sed gravida. Proin ornare elementum scelerisque. Donec sed condimentum lacus. Donec auctor euismod mauris. Phasellus ultrices in velit eu varius. Quisque aliquam odio ex, sed feugiat lorem pellentesque ac. Morbi consectetur dictum blandit.',
+    description: 'As a software designer with Nokia, I had the pleasure to take part in multiple roles. Our Teams main responsibility was feature implementation for ESM (Subscriber Management) which involved working with the SAM client. The SAM client is Java GUI based application designed to help clients manage their nodes (routers etc) with ease and accessibility. The code behind the SAM client involved two main languages. Java and XML. ',
     tasks: [
         {
             title: 'Web Dev',
@@ -83,7 +83,7 @@ export const SIGNIANT = {
     date: 'Jan 2017 - Apr 2017',
     logo: signiantLogo,
     image: signiantImage,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra dignissim convallis. Phasellus vehicula augue arcu, quis eleifend erat commodo ac. Suspendisse eget euismod est. Ut pharetra volutpat mi sed gravida. Proin ornare elementum scelerisque. Donec sed condimentum lacus. Donec auctor euismod mauris. Phasellus ultrices in velit eu varius. Quisque aliquam odio ex, sed feugiat lorem pellentesque ac. Morbi consectetur dictum blandit.',
+    description: 'At Signiant I was apart of Team Voyager which consisted of around 5-6 members.  My role consisted of various tasks but primarily it was work on the Signiant App (C++), Signiant’s Media Shuttle Cloud (Java), and The Transfer Api (Javascript). I also had the pleasure to design a web application that facilitated testing for all developers at the company. As well as development experience, I had the opportunity to get fully trained in agile work flow, attend daily scrums, and participate in bi-weekly iteration demos. ',
     tasks: [
         {
             title: 'Web Dev',
@@ -107,7 +107,7 @@ export const TELFER = {
     date: 'Jun 2016 - Sep 2016',
     logo: telferLogo,
     image: telferImage,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra dignissim convallis. Phasellus vehicula augue arcu, quis eleifend erat commodo ac. Suspendisse eget euismod est. Ut pharetra volutpat mi sed gravida. Proin ornare elementum scelerisque. Donec sed condimentum lacus. Donec auctor euismod mauris. Phasellus ultrices in velit eu varius. Quisque aliquam odio ex, sed feugiat lorem pellentesque ac. Morbi consectetur dictum blandit.',
+    description: 'My role was to work on the CV System. This CV system is used by Professors at the Telfer School of Management to create their own resumes and present their credentials to those who are interested in viewing them, as well as assisting in the school’s accreditation process. During my time here I designed the user documentation for this new CV system by using the old user guide as a reference. I also was able to help develop certain features using PHP and test this new system by the guidance of the Lead Programmer.',
     tasks: [
         {
             title: 'Web Dev',
@@ -136,7 +136,7 @@ export const MUNCHIES = {
     stack: 'React.js / Flask',
     logo: munchiesLogo,
     image: munchiesImage,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra dignissim convallis. Phasellus vehicula augue arcu, quis eleifend erat commodo ac. Suspendisse eget euismod est. Ut pharetra volutpat mi sed gravida. Proin ornare elementum scelerisque. Donec sed condimentum lacus. Donec auctor euismod mauris. Phasellus ultrices in velit eu varius. Quisque aliquam odio ex, sed feugiat lorem pellentesque ac. Morbi consectetur dictum blandit.',
+    description: 'Munchies is a web application designed for my database design course which allows users to view the ratings and menus of restaurants. Because the application was to be designed in the scope of the course the database was populated with restaurants locally. The website was designed by focusing on finding what users want to eat fast and in a beautiful way. Once the user searches for a type of food or restaurant the website will bring them to a restaurant page that shows all the relevant information including the menu.',
 }
 
 export const GRADED = {
@@ -145,5 +145,5 @@ export const GRADED = {
     stack: 'Android',
     logo: gradedLogo,
     image: gradedImage,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra dignissim convallis. Phasellus vehicula augue arcu, quis eleifend erat commodo ac. Suspendisse eget euismod est. Ut pharetra volutpat mi sed gravida. Proin ornare elementum scelerisque. Donec sed condimentum lacus. Donec auctor euismod mauris. Phasellus ultrices in velit eu varius. Quisque aliquam odio ex, sed feugiat lorem pellentesque ac. Morbi consectetur dictum blandit.',
+    description: 'Graded is a android application designed to help students keep track of their grades throughout the semester. It was designed with the user experience in mind and because of this the app was kept very simple and user friendly. The application was the final project of my User Experience course and allowed my partner and I to do user research on fellow students, mock up designs and finally code the app in Android Studio. As university students the most important aspect of our lives often revolve around making sure our studies and grades are kept up. Often times because of the many different deliverables and items each course offers it becomes difficult to manage what needs to be done and gauge how one is really doing in each course.',
 }
